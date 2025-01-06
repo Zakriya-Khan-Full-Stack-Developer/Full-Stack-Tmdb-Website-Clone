9deeed5bfde99f3ff8019eddfbfdf736582bc21b
Zakriya-Khan-Full-Stack-Developer <zk145212128@gmail.com>
2025-01-06 21:39:14 +0500

    backend url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000/api/v3/tmdb', // Backend URL
+        target: 'https://full-stack-tmdb-website-clone.vercel.app',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''), // Optional path rewrite
+        rewrite: (path) => path.replace(/^\/api/, '/api'),
       },
     },
   },
