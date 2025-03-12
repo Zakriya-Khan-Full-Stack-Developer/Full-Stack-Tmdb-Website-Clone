03941e8bca2851a5cee43ed4cbab4eb8a92531d3
Zakriya khan <zk145212128@gmail.com>
2025-03-12 18:37:42 +0500

    Update App.jsx
    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,17 +19,8 @@ import LeaderBoardDetail from "./components/LeaderBoard/LeaderBoardDetail.jsx";
 import "./App.css"; // Adjust the path if necessary
 
 // Redirect Component
-const RedirectToOffer = () => {
-  React.useEffect(() => {
-    window.location.href = "https://ururgisha.net/4/8754602?var=your_source_id";
-  }, []);
-
   return (
-    <div style={{ textAlign: "center", marginTop: "20px" }}>
-      <h2>Redirecting you to the offer...</h2>
-    </div>
-  );
-};
+  
 
 const App = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false); // Manage the toggle state for the menu
@@ -39,54 +30,7 @@ const App = () => {
   return (
     <Router>
       <div>
-        {/* Banner Section */}
-        <a
-          href="https://thadolurgaux.net/4/8754595?var=idd"
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{
-            display: "block",
-            textAlign: "center",
-            margin: "20px 0",
-          }}
-        >
-          <img
-            src="/background1.webps"
-            alt="Special Offer"
-            style={{
-              maxWidth: "100%",
-              height: "auto",
-              border: "2px solid #ccc",
-              borderRadius: "8px",
-            }}
-          />
-        </a>
-
-        {/* Direct Link Classic Banner */}
-        <a
-          href="https://ururgisha.net/4/8754602?var=idd"
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{
-            display: "block",
-            textAlign: "center",
-            margin: "20px 0",
-          }}
-        >
-          <img
-            src="/background1.webpss"
-            alt="Check Out This Offer"
-            style={{
-              maxWidth: "100%",
-              height: "auto",
-              border: "2px solid #ccc",
-              borderRadius: "8px",
-            }}
-          />
-        </a>
-
-        {/* Header with Hamburger Menu for Small Screens */}
-        <Header toggleMenu={toggleMenu} isMenuOpen={isMenuOpen} />
+     
 
         <ToastContainer
           position="top-right"
